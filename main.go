dc29c783bc96bda4be2607fc2080b1643f4cd54b
DoJun Park <dojun527@naver.com>
2021-02-24 14:11:31 +0900

    update main.go mydict.go

--- main.go
+++ main.go
@@ -32,17 +32,46 @@ func main() {
 	// }
 
 	// Add
+	// dictionary := mydict.Dictionary{}
+	// word := "hello"
+	// definition := "greeting"
+	// err := dictionary.Add(word, definition)
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// hello, _ := dictionary.Search(word)
+	// fmt.Println(hello)
+	// err2 := dictionary.Add(word, definition)
+	// if err2 != nil {
+	// 	fmt.Println(err2)
+	// }
+	// //------------------------------------------------------
+
+	// Update
+	// dictionary := mydict.Dictionary{}
+	// baseWord := "hello"
+	// dictionary.Add(baseWord, "Firsts")
+	// err := dictionary.Update(baseWord, "Second")
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// word, _ := dictionary.Search(baseWord)
+	// fmt.Println(word)
+
+	// Delete
 	dictionary := mydict.Dictionary{}
-	word := "hello"
-	definition := "greeting"
-	err := dictionary.Add(word, definition)
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
 
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println(word)
 	}
-	hello, err := dictionary.Search(word)
-	fmt.Println(hello)
-
-	// //------------------------------------------------------
-
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -6,8 +6,11 @@ import "errors"
 type Dictionary map[string]string
 
 //return에 적는 것보다 이렇게 변수로 선언하는 것이 좋음
-var errNotFound = errors.New("not found")
-var errWordExists = errors.New("That word already exists")
+var (
+	errNotFound   = errors.New("Not Found")
+	errWordExists = errors.New("That word already exists")
+	errCantUpdate = errors.New("Cant update non-existing word")
+)
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -20,21 +23,40 @@ func (d Dictionary) Search(word string) (string, error) {
 
 // Add a word to the dictionary
 func (d Dictionary) Add(word string, def string) error {
-	_, err := d.Search((word))
+	_, err := d.Search(word)
 
 	// 방법 1
 	switch err {
 	case errNotFound:
 		d[word] = def
-	case errWordExists:
+	case nil:
 		return errWordExists
-		return nil
 	}
+	return nil
 
 	// // 방법 2
-	// if err == errNotFound { //dictionary안에 추가하려는 단어가 없을 때
+	// if err == errNtFound { //dictionary안에 추가하려는 단어가 없을 때
 	// 	d[word] = def
 	// } else if err == nil {
 	// 	return errWordExists
 	// }
+}
+
+// Update a word
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+// Delete a word
+func (d Dictionary) Delete(word string) {
+	// delete함수를 사용할 수 있는데 아무 값도 return하지 않고 첫번째 인자에는 삭제하려는 맵이름, 두번째 인자에는 key값
+	delete(d, word)
+
 }
